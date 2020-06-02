bf8a1a675759d29ea5df1b0e37e32ec162cd0696
dcdourado <dcdourado@gmail.com>
2020-06-02 08:43:57 -0300

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import Lobby from './Components/Lobby';
+
+import './App.scss';
+
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Lobbyzada</h1>
+      <div className="app__lobbies">
+        <Lobby playerNumber={0} maxCap={10} />
+        <Lobby playerNumber={1} maxCap={10} />
+        <Lobby playerNumber={2} maxCap={10} />
+        <Lobby playerNumber={3} maxCap={10} />
+      </div>
     </div>
   );
-}
+};
 
 export default App;
